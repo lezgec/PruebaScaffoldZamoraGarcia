a22327c60c3753c40cfcb237cdf53055e7135c4b
lzamora <lzamora@LuisZamora>
2024-11-24 06:37:34 +0000

    Add project files.

---
+++ PruebaScaffoldLZ/Models/ApplicationDbContext.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace PruebaScaffoldLZ.Models;
+
+public partial class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext()
+    {
+    }
+
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Estudiante> Estudiantes { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Server=localhost\\SQLEXPRESS;Database=ScaffoldLz;Trusted_Connection=True;TrustServerCertificate=True;");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Estudiante>(entity =>
+        {
+            entity.HasKey(e => e.EstudianteId).HasName("PK__Estudian__6F7682D8D829F3F0");
+
+            entity.ToTable("Estudiante");
+
+            entity.HasIndex(e => e.Correo, "UQ__Estudian__60695A19AD54F8E1").IsUnique();
+
+            entity.Property(e => e.Activo).HasDefaultValue(true);
+            entity.Property(e => e.Apellido).HasMaxLength(50);
+            entity.Property(e => e.Correo).HasMaxLength(100);
+            entity.Property(e => e.Nombre).HasMaxLength(50);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ PruebaScaffoldLZ/Models/Estudiante.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PruebaScaffoldLZ.Models;
+
+public partial class Estudiante
+{
+    public int EstudianteId { get; set; }
+
+    public string Nombre { get; set; } = null!;
+
+    public string Apellido { get; set; } = null!;
+
+    public string Correo { get; set; } = null!;
+
+    public DateOnly FechaNacimiento { get; set; }
+
+    public bool? Activo { get; set; }
+}
